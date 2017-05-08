54a3251b75189a25727c5c7caff4ba692eacc147
pablit07 <paul.kohlhoff@gmail.com>
2017-05-08 02:58:29 +0000

    adding magento files

---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_ffok1a',
+  ),
+  'crypt' => 
+  array (
+    'key' => '7f3145b7d34170607149df2a45961306',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'fbdb.czs81xeopspv.us-west-2.rds.amazonaws.com',
+        'dbname' => 'magento',
+        'username' => 'root',
+        'password' => 'BBu1w101NSPm',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'customer_notification' => 0,
+    'full_page' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sun, 16 Apr 2017 15:48:12 +0000',
+  ),
+);
